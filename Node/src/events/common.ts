10fe22404c9a3231ea30660679ab69d820053c36
Azshurith <r.nylzon@gmail.com>
2024-04-24 22:41:32 +0800

    Better docker configuration

---
+++ Node/src/events/common.ts
@@ -0,0 +1,12 @@
+import type { ArgsOf } from "discordx";
+import { Discord, On } from "discordx";
+
+@Discord()
+export class Example {
+
+    @On()
+    messageCreate([message]: ArgsOf<"messageCreate">): void {
+        console.log(message.author.username, "said:", message.content);
+    }
+
+}
